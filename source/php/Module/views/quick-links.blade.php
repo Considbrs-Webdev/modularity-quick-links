15e4caac311d129c2fa3f99d8037865b1e9cdc65
Michael Claesson <michael.claesson@consid.se>
2025-12-22 19:20:36 +0100

    Responsive

--- source/php/Module/views/quick-links.blade.php
+++ source/php/Module/views/quick-links.blade.php
@@ -1,6 +1,6 @@
 <div class="modularity-quick-links" data-max-items="{{ $maxItemsPerRow }}"
     data-large-icons="{{ $largeIcons ? 'true' : 'false' }}"
-    data-use-description="{{ $useShortDescription ? 'true' : 'false' }}">
+    data-use-description="{{ $useShortDescription ? 'true' : 'false' }}" data-item-count="{{ count($links) }}">
     <div class="modularity-quick-links__grid">
         @foreach ($links as $linkItem)
             <a href="{{ $linkItem['link'] }}"
